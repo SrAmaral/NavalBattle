210694930f1c689f386d9c604f248bcd4cafc29f
SrAmaral <edu.amaral8@gmail.com>
2021-12-30 09:45:29 -0300

    alteraçao

--- src/br/com/Batalha/Naval/Main.java
+++ src/br/com/Batalha/Naval/Main.java
@@ -3,6 +3,8 @@ package br.com.Batalha.Naval;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("text");
+        System.out.println("text2");
     }
 }
